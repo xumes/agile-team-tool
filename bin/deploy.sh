8341825c1baa5102688ec4b97fd2a7e4b3d12442
Michael D. Ramos <ramosm@us.ibm.com>
2016-07-14 09:30:57 -0400

    Update deploy.sh
    Adding back in the blue green changes.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,7 +7,7 @@ curl -sLO $agileManifestGreenURL
 ./cf push agile-tool-nodejs-green-stage -f agile-team-tool-green-manifest.yml
 ./cf map-route agile-tool-nodejs-green-stage mybluemix.net -n agile-tool-nodejs-stage
 ./cf unmap-route agile-tool-nodejs-stage mybluemix.net -n agile-tool-nodejs-stage
-./cf push agile-tool-nodejs-stage -f agile-team-tool-manifest.yml
-./cf map-route agile-tool-nodejs-stage mybluemix.net -n agile-tool-nodejs-stage
-./cf unmap-route agile-tool-nodejs-green-stage mybluemix.net -n agile-tool-nodejs-stage
-./cf delete agile-tool-nodejs-green-stage -f
+./cf stop agile-tool-nodejs-stage
+./cf rename agile-tool-nodejs-green-stage agile-tool-nodejs-stage-temp
+./cf rename agile-tool-nodejs-stage agile-tool-nodejs-green-stage
+./cf rename agile-tool-nodejs-stage-temp agile-tool-nodejs-stage
