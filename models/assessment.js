1c59a36b1c0dd7aaad88fc9603481524d3a0d23f
Jackelyn Mondigo <mondigjd@ph.ibm.com>
2016-07-14 22:28:45 +0800

    Common cloudant calls

--- models/assessment.js
+++ models/assessment.js
@@ -1,2 +1,22 @@
-/* Put cloudant calls for assessment here */
+/* Put cloudant calls for team here */
+var common = require('./common-cloudant');
 
+exports.getTeamAssessments = function(teamId, callback) {
+  common.getByViewKey('agile', 'maturityAssessmentResult', teamId, callback);
+};
+
+exports.getAssessmentTemplate = function(callback) {
+  common.getByView('agile', 'maturityAssessment', teamId, callback);
+};
+
+exports.addTeamAssessment = function(data, callback) {
+  common.addRecord(data, callback);
+};
+
+exports.updateTeamAssessment = function(data, callback) {
+  common.updateRecord(data, callback);
+};
+
+exports.deleteAssessment = function(_id, _rev, callback) {
+  common.deleteRecord(_id, _rev, callback);
+};
