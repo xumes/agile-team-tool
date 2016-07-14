20b12670efab08f7325a5b06a772deb3139ad076
Colby Scotta <cjscotta@us.ibm.com>
2016-07-14 13:21:40 -0400

    added logout

--- public/j/header.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
- 	//nav bar drop down menu display
-	$('#dropdown-display-li').on('mouseenter', function() {
-		$('#dropdown-display-li').attr("class","ibm-masthead-item-signin ibm-active");
-	}).on('mouseleave', function(){
-		$('#dropdown-display-li').attr("class","ibm-masthead-item-signin");
-	});
-});
