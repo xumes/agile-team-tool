ba5f59ed32535d965723f223c491d746d00e616f
Colby Scotta <cjscotta@us.ibm.com>
2016-07-14 11:44:19 -0400

    removing un-needed code for dropdown hover

--- public/j/header.js
+++ public/j/header.js
@@ -1,21 +1,8 @@
 $( document ).ready(function() {
- 
+ 	//nav bar drop down menu display
 	$('#dropdown-display-li').on('mouseenter', function() {
-	     
 		$('#dropdown-display-li').attr("class","ibm-masthead-item-signin ibm-active");
-
 	}).on('mouseleave', function(){
-	     
 		$('#dropdown-display-li').attr("class","ibm-masthead-item-signin");
 	});
- 
-	$('#ibm-signin-minimenu-container').on('mouseenter', function() {
-	     
-		$('#dropdown-display-li').attr("class","ibm-masthead-item-signin ibm-active");
-
-	}).on('mouseleave', function(){
-	     
-		$('#dropdown-display-li').attr("class","ibm-masthead-item-signin");
-	});
-
 });
