288e6c3f0181ce3ba4dd1fe58b20b55b8af955c8
Metaly Herana <heranaml@ph.ibm.com>
2016-07-16 01:23:07 +0800

    added team api endpoint for getting names and roles

--- routes/api/teams.js
+++ routes/api/teams.js
@@ -7,11 +7,33 @@ module.exports = function(app, includes) {
     teamId = req.params.teamId;
     teamModel.getTeam(teamId, function(err, result){
       if(err)
-        res.status(500).send({ error: err });
+        return res.status(500).send({ error: err });
       else
-        res.send(result)
+        return res.send(result);
     });
   };
+
+  getTeamRole = function(req, res){
+    teamModel.getRole(function(err, result){
+      if(err)
+        return res.status(500).send({ error: err });
+      else
+        return res.send(result);
+    });
+  };
+
+getTeamName = function(req, res){
+    teamModel.getName(function(err, result){
+      if(err)
+        return res.status(500).send({ error: err });
+      else
+        return res.send(result);
+    });
+  };
+
+  app.get('/api/teams/roles', [includes.middleware.auth.requireLogin], getTeamRole);
+
+  app.get('/api/teams/names', [includes.middleware.auth.requireLogin], getTeamName);
 
   app.get('/api/teams/:teamId?', [includes.middleware.auth.requireLogin], getTeam);
 
