fb917a96e847dd41eaabf79a61773aaef3a4ec6a
John Revano <john.elden.revano@ibm.com>
2016-07-26 17:08:42 -0400

    Fix API team tests

--- routes/api/login.js
+++ routes/api/login.js
@@ -15,31 +15,22 @@ module.exports = function(app, includes) {
 
   // test login function
   app.get('/api/login/masquerade/:user', function(req, res) {
-    //if (req.headers['test-api-key'] == "test-secrect") {
-      //console.log("pass test");
-      //req.session['email'] = "winnuser@us.ibm.com";
-      var user = {
-          'shortEmail': req.params.user,
-          'ldap':
-            {
-              'serialNumber': '123456PH1',
-              'hrFirstName': 'John',
-              'hrLastName': 'Doe'
-            }
-      };
-      req.session.user = user;
-      console.log(req.session.user);
-
-      //req.session['user'] = user;
-      req.login(user, function(err) {
-        if (err) {
-          res.send(err);
-        } else {
-          res.status(200).send('Successfully authenticated test user');
+    var user = {
+      'shortEmail': req.params.user,
+      'ldap':
+        {
+          'serialNumber': '123456PH1',
+          'hrFirstName': 'John',
+          'hrLastName': 'Doe'
         }
-      });
-    //} else {
-      //res.status(401).send('Invalid user');
-    //}
+    };
+    req.login(user, function(err) {
+      if (err) {
+        res.send(err);
+      } else {
+        req.session.user = user;
+        res.status(200).send('Successfully authenticated test user');
+      }
+    });
   });
 };
