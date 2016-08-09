87d9c6ae32d8424551784a2d3ee8e4d0be5806d6
Colby Scotta <cjscotta@us.ibm.com>
2016-08-09 16:23:59 -0400

    added load all source design docs and load all cloudant design docs using promises so that we can then compare them

--- cloudant/init.js
+++ cloudant/init.js
@@ -1,8 +1,61 @@
-var logger   = require('../middleware/logger');
-var cloudant = require('cloudant');
+var Promise       = require('bluebird');
+var logger        = require('../middleware/logger');
+var settings      = require('../settings');
+var Cloudant      = require('cloudant');
+var cloudantDb    = Cloudant(settings.cloudant.url);
+var dbName        = settings.cloudant.dbName;
+var db            = cloudantDb.use(dbName);
+var fs            = require('fs');
+var _             = require('underscore');
+var async         = require('async');
 
 //TODO
 module.exports.init = function(){
-  logger.get('init').info("Checking Cloudant DB fixtures...");
+  //only init cloudant if we are running locally or on app instance 0 in cloudfoundry
+  if(_.isEmpty(process.env.CF_INSTANCE_INDEX) || process.env.CF_INSTANCE_INDEX == "0"){
+    logger.get('init').info("Checking Cloudant DB fixtures...");
+    
+    Promise.join(getCloudantDocs(), getSourceDocs(),
+        function(cloudantDocs, sourceDocs) {
+          
+        //TODO
+        // console.log(cloudantDocs);
+        // console.log("\n\n\n\n\n");
+        // console.log(sourceDocs);
+
+    });
+      
+  }
   
+}
+
+var getCloudantDocs = function(){
+  return new Promise(function(resolve, reject) {
+    db.list({startkey:'_design', endkey:"_design0", include_docs:true},
+      function(err, body){
+        if (err) 
+          reject(err);
+        else
+          resolve(body.rows);
+      });
+  });
+}
+
+var getSourceDocs = function(){
+  return new Promise(function(resolve, reject) {
+    fs.readdir("./cloudant/design_docs", function(err, files){
+      if(err)
+        reject(err);
+        
+      var res = [];
+      async.each(files, function(file, callback) {
+         var designDoc = require("./design_docs/" + file);
+         res.push(designDoc);
+         callback();
+          
+      }, function() {
+         resolve(res);
+       });
+    });
+ });
 }
