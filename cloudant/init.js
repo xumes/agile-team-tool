516bcc07194f003b5c116d0384e64b6ff42473a0
Yanliang Gu <yanliang@yanliangs-mbp.somers.ibm.com>
2016-08-11 12:05:26 -0400

    fixed the bug cloudant init load the ".DS_Store" file on the local

--- cloudant/init.js
+++ cloudant/init.js
@@ -13,7 +13,7 @@ module.exports.init = function(){
   //only init cloudant if we are running locally or on app instance 0 in cloudfoundry
   if(_.isEmpty(process.env.CF_INSTANCE_INDEX) || process.env.CF_INSTANCE_INDEX == "0"){
     logger.get('init').info("Checking Cloudant DB design docs and indexes...");
-    
+
     //design docs
     Promise.join(getCloudantDocs(), getSourceDocs(), function(cloudantDocs, sourceDocs) {
         /*ignore if design doc exists in the DB but not in the source
@@ -21,12 +21,12 @@ module.exports.init = function(){
         logger.get('init').info("Cloudant design docs:"+cloudantDocs.length
         +", "+"Source design docs:"+sourceDocs.length);
         //for each source doc, find the doc in cloudant
-        _.each(sourceDocs, function(sDoc){ 
+        _.each(sourceDocs, function(sDoc){
           var cDoc = _.find(cloudantDocs, function(cDoc){return _.isEqual(sDoc._id, cDoc.doc._id); });
           //not found in cloudant, so create it in cloudant
           if(_.isEmpty(cDoc)){
             logger.get('init').warn(sDoc._id + " NOT found in DB. Creating it now..");
-            
+
             db.insert(sDoc, function(err, body) {
               if(!err)
                 logger.get('init').info("Create Success: "+sDoc._id);
@@ -52,7 +52,7 @@ module.exports.init = function(){
 
         });
     });
-    
+
     //indexes
     getSourceIndexes()
     .then(function(indexes){
@@ -62,9 +62,9 @@ module.exports.init = function(){
             logger.get('init').error(err);
           else{
             if(_.isEqual(response.result,"exists"))
-              logger.get('init').info(index.name+" already exists");        
+              logger.get('init').info(index.name+" already exists");
             else
-              logger.get('init').info("Index: "+index.name+" creation result: "+response.result);        
+              logger.get('init').info("Index: "+index.name+" creation result: "+response.result);
           }
         });
       });
@@ -76,7 +76,7 @@ var getCloudantDocs = function(){
   return new Promise(function(resolve, reject) {
     db.list({startkey:'_design', endkey:"_design0", include_docs:true},
       function(err, body){
-        if (err) 
+        if (err)
           reject(err);
         else
           resolve(body.rows);
@@ -89,9 +89,15 @@ var getSourceDocs = function(){
     fs.readdir("./cloudant/design_docs", function(err, files){
       if(err)
         reject(err);
-        
+      for (var i = 0; i < files.length; i++) {
+        if(files[i].substring(files[i].length - 4, files[i].length) != 'json') {
+          files.splice(i,1);
+        }
+      }
+      console.log(files);
       var res = [];
       async.each(files, function(file, callback) {
+
          var designDoc = require("./design_docs/" + file);
          res.push(designDoc);
          callback();
@@ -108,7 +114,11 @@ var getSourceIndexes = function(){
     fs.readdir("./cloudant/indexes", function(err, files){
       if(err)
         reject(err);
-        
+        for (var i = 0; i < files.length; i++) {
+          if(files[i].substring(files[i].length - 4, files[i].length) != 'json') {
+            files.splice(i,1);
+          }
+        }
       var res = [];
       async.each(files, function(file, callback) {
          var index = require("./indexes/" + file);
