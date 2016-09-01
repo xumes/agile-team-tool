619b463d1df96cb23e60183def6e8552f49f3072
Yanliang Gu <yanliang@yanliangs-mbp.somers.ibm.com>
2016-09-01 11:47:09 -0400

    test case coverage

--- test/routes/api/snapshot.js
+++ test/routes/api/snapshot.js
@@ -58,86 +58,28 @@ describe('Snapshot API Test [GET /api/snapshot/getrollupdata]: calculate and upd
      });
   });
 
-  // it('Successfully get roll up data', function(done){
-  //   var query = {
-  //     'startTime' : '03/18/2016',
-  //     'endTime' : '08/18/2016'
-  //   };
-  //   var req = request(app).get('/api/snapshot/getrollupdata?' + querystring.stringify(query));
-  //   agent.attachCookies(req);
-  //   req.end(function(err, res){
-  //     if (err) {
-  //     } else {
-  //       expect(res.statusCode).to.be.equal(200);
-  //     }
-  //     done();
-  //   });
-  // });
-  //
-  // it('failed because lack of startTime', function(done){
-  //   var query = {
-  //     'endTime' : '08/18/2016'
-  //   };
-  //   var req = request(app).get('/api/snapshot/getrollupdata?' + querystring.stringify(query));
-  //   agent.attachCookies(req);
-  //   req.end(function(err, res){
-  //     if (err) {
-  //     } else {
-  //       expect(res.statusCode).to.be.equal(400);
-  //       expect(res.body.error).to.be.equal('missing start time');
-  //     }
-  //     done();
-  //   });
-  // });
-  //
-  // it('failed because lack of endTime', function(done){
-  //   var query = {
-  //     'startTime' : '03/18/2016'
-  //   };
-  //   var req = request(app).get('/api/snapshot/getrollupdata?' + querystring.stringify(query));
-  //   agent.attachCookies(req);
-  //   req.end(function(err, res){
-  //     if (err) {
-  //     } else {
-  //       expect(res.statusCode).to.be.equal(400);
-  //       expect(res.body.error).to.be.equal('missing end time');
-  //     }
-  //     done();
-  //   });
-  // });
-  //
-  // it('failed because endTime is after current time', function(done){
-  //   var query = {
-  //     'startTime' : '03/18/2016',
-  //     'endTime' : '03/18/2999'
-  //   };
-  //   var req = request(app).get('/api/snapshot/getrollupdata?' + querystring.stringify(query));
-  //   agent.attachCookies(req);
-  //   req.end(function(err, res){
-  //     if (err) {
-  //     } else {
-  //       expect(res.statusCode).to.be.equal(400);
-  //       expect(res.body.error).to.be.equal('end time is after current time');
-  //     }
-  //     done();
-  //   });
-  // });
-  //
-  // it('failed because endTime is after current time', function(done){
-  //   var query = {
-  //     'startTime' : '03/18/2016',
-  //     'endTime' : '02/18/2016'
-  //   };
-  //   var req = request(app).get('/api/snapshot/getrollupdata?' + querystring.stringify(query));
-  //   agent.attachCookies(req);
-  //   req.end(function(err, res){
-  //     if (err) {
-  //     } else {
-  //       expect(res.statusCode).to.be.equal(400);
-  //       expect(res.body.error).to.be.equal('start time is after end time');
-  //     }
-  //     done();
-  //   });
-  // });
+  it('Successfully return roll up data for team', function(done){
+    var req = request(app).get('/api/snapshot/rollupdatabyteam/'+'ag_team_CIO');
+    agent.attachCookies(req);
+    req.end(function(err, res){
+      if (err) {
+      } else {
+        expect(res.statusCode).to.be.equal(200);
+      }
+      done();
+    });
+  });
+
+  it('Successfully return roll up squads for team', function(done){
+    var req = request(app).get('/api/snapshot/rollupsquadsbyteam/'+'ag_team_CIO');
+    agent.attachCookies(req);
+    req.end(function(err, res){
+      if (err) {
+      } else {
+        expect(res.statusCode).to.be.equal(200);
+      }
+      done();
+    });
+  });
 
 });
