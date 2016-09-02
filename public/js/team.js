c64a7d91b7ab083bfbdb201046869cb669a45a2c
descalhc <descalhc@ph.ibm.com>
2016-09-02 08:48:23 +0800

    squad team indicator on child teams

--- public/js/team.js
+++ public/js/team.js
@@ -679,7 +679,7 @@ function loadTeamChildren(currentId) {
             row = row + "<label for='child_" + index + "' class='ibm-access'>Select " + childTeamName + "</label>";
             row = row + "</td>";
             row = row + "<td id='ref_id_" + childTeamId + "'>" + childTeamName + "</td>";
-            row = row + "<td>" + childSquadIndicator + "</td>";
+            row = row + "<td id='ref_squadteam_" + index + "'>" + childSquadIndicator + "</td>";
             row = row + "<td id='ref_desc_"+index+"'>" + childTeamDesc + "</td>";
             row = row + "</tr>";
             $("#childrenList").append(row);
@@ -698,6 +698,7 @@ function loadTeamChildren(currentId) {
 function getTeamDetailHandler(index, team) {
   if (team != null) {
     $("#ref_desc_" + index).text(team.desc);
+    $("#ref_squadteam_" + index).text(team.squadteam);
     updateAgileTeamCache(team);
   } else {
     $("#ref_desc_" + index).text("-unavailable-");
