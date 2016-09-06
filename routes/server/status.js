1dad7f520e4963bcedb540fe63889ae1ea7bacd0
John Revano <john.elden.revano@ibm.com>
2016-09-06 15:27:49 -0400

    Fix SSO logout by breaking the loop and adding a new status page

---
+++ routes/server/status.js
@@ -0,0 +1,26 @@
+var settings = require('../../settings');
+
+module.exports = function(app, includes) {
+  var render = includes.render;
+
+  showHome = function(req, res) {
+    var json =
+    {
+      'pageTitle'          : 'Home',
+      'googleAnalyticsKey' : settings.googleAnalyticsKey
+    };
+    render(req, res, 'home', json);
+  };
+
+  showSSOLogoutPage = function(req, res) {
+    var json =
+    {
+      'pageTitle': 'Logout',
+      'googleAnalyticsKey' : settings.googleAnalyticsKey,
+      'message': 'You have succesfully logged out, please close this browser window.'
+    };
+    render(req, res, 'status', json);
+  };
+
+  app.get('/ssologout', showSSOLogoutPage);
+};
