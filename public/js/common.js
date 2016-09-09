1b2ed63eb11a16b6f50d4a151b5b17bb87659f90
Jean C. Lam <jeanlam@us.ibm.com>
2016-09-09 09:21:28 -0500

    outage message changes on dynamicChange

--- public/js/common.js
+++ public/js/common.js
@@ -41,7 +41,7 @@ function getPageVariables(page, _callback) {
 
 function setSystemMessage(systemStatusControl, systemStatusMsg){
 	//set db system message on the top of the page banner - header.ejs id=systMsg
-  if (systemStatusControl == 'AdminOnlyChange' || systemStatusControl == 'AdminOnlyReadChange'){
+  if (systemStatusControl == 'DynamicChange' || systemStatusControl == 'AdminOnlyChange' || systemStatusControl == 'AdminOnlyReadChange'){
     $('#warningBar').html(systemStatusMsg);
     $('#warningBar').show();
   } else {
