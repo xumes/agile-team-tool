299a486958cdbab4b8df4ba81aad218a9f2bbaab
Jean C. Lam <jeanlam@us.ibm.com>
2016-09-20 17:07:03 -0500

    eliminate single quote input

--- public/js/iteration.js
+++ public/js/iteration.js
@@ -162,14 +162,11 @@ jQuery(function($) {
   $('#fteThisiteration, #clientSatisfaction, #teamSatisfaction, #cycleTimeWIP, #cycleTimeInBacklog').keypress(function(evt) {
     evt = (evt) ? evt : window.event;
     var charCode = (evt.which) ? evt.which : evt.keyCode;
-    if (charCode == 8 || charCode == 37 || charCode == 39) {
+    if (charCode == 8 || charCode == 37) {
       return true;
     } else if (charCode == 46 && $(this).val().indexOf('.') != -1) {
       return false;
-
     } else if (charCode > 31 && charCode != 46 && (charCode < 48 || charCode > 57)) {
-      return false;
-    } else if (charCode == 222) {
       return false;
     }
     return true;
