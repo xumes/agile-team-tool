35ff4bdb167b2ee8680847366b5dff842b6616f4
Yanliang Gu <Yanliang.Gu1@ibm.com>
2016-10-04 10:10:21 -0400

    snapshot api test rewrote

--- test/routes/api/snapshot.js
+++ test/routes/api/snapshot.js
@@ -1,12 +1,53 @@
 var chai = require('chai');
 var expect = chai.expect;
 var app = require('../../../app');
+var iterationModel = require('../../../models/mongodb/snapshot');
 var request = require('supertest');
 var _ = require('underscore');
-var util = require('../../../helpers/util');
 var querystring = require('querystring');
 var agent = request.agent(app);
-var userEmail = 'john.doe@ph.ibm.com';
+var testUser = {
+  'userId': 'TEST1234567',
+  'name': 'test user',
+  'email': 'testuser@test.com',
+  'adminAccess': 'none'
+};
+
+describe('Snapshot API Test', function(){
+  before(function(done){
+    var promiseArray = [];
+    promiseArray.push(userModel.delete(testUser.email));
+    Promise.all(promiseArray)
+      .then(function(results){
+        return userModel.create(testUser);
+      })
+      .then(function(result){
+        done();
+      })
+      .catch(function(err){
+        done();
+      });
+  });
+  before(function(done) {
+    agent
+      .get('/api/login/masquerade/' + testUser.email)
+      .send()
+      .end(function(err, res) {
+        if (err) throw err;
+        agent.saveCookies(res);
+        done();
+      });
+  });
+  describe('Snapshot model [updateRollUpData]', function(){
+    it('return successful for updating roll up data', function(done) {
+      snapshotModel.updateRollUpData()
+        .then(function(result){
+          expect(result).to.be.a('string');
+          done();
+        });
+    });
+  });
+});
 
 describe('Snapshot API Test [GET /api/snapshot/getrollupdata]: calculate and update non squad teams iteartions', function() {
   this.timeout(60000);
