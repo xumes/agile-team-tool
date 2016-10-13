1d767cc1b050d90b262d1ca9361b4e7c4ce15600
Yanliang Gu <Yanliang.Gu1@ibm.com>
2016-10-13 12:49:37 -0400

    tab view in home page

--- public/js/components/HomeNavTab.jsx
+++ public/js/components/HomeNavTab.jsx
@@ -2,18 +2,39 @@ var React = require('react');
 var api = require('../api.jsx');
 
 var HomeNavTab = React.createClass({
-  myTeamsClicked: function() {
-    if (this['data-state'] != 'open') {
+  getInitialState: function() {
+    return {
+      myTeamsState: 'open',
+      allTeamsState: ''
     }
   },
+
+  myTeamsClicked: function() {
+    if (this.state.myTeamsState != 'open') {
+      this.setState({'myTeamsState': 'open'});
+      this.setState({'allTeamsState': ''});
+    }
+  },
+
+  allTeamsClicked: function() {
+    if (this.state.allTeamsState != 'open') {
+      this.setState({'myTeamsState': ''});
+      this.setState({'allTeamsState': 'open'});
+    }
+  },
+
   render: function() {
     var tabStyle = {
       'width': '100%'
     };
+
+    var myTeamsState = this.state.myTeamsState;
+    var allTeamsState = this.state.allTeamsState;
+
     return (
       <ul class='tab-ul' role='tablist' style={tabStyle}>
-          <li id='myTeams' class='tab-ul--item' role='tab' data-state='open' tabIndex='0' onClick={this.myTeamsClicked}>My teams</li>
-          <li id='allTeams' class='tab-ul--item' role='tab'>All teams</li>
+          <li id='myTeams' class='tab-ul--item' role='tab' data-state={myTeamsState} tabIndex='0' onClick={this.myTeamsClicked}>My teams</li>
+          <li id='allTeams' class='tab-ul--item' role='tab' data-state={allTeamsState} onClick={this.allTeamsClicked}>All teams</li>
       </ul>
     )
   }
