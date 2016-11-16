fa1bd7a283dab381268e83208ddcd206e0db8124
Jackelyn Mondigo <mondigjd@ph.ibm.com>
2016-11-16 18:55:12 +0800

    repo merge

--- public/js/components/assessment/AssessmentActiveTemplates.jsx
+++ public/js/components/assessment/AssessmentActiveTemplates.jsx
@@ -34,27 +34,29 @@ var Level = React.createClass({
     */
     render: function(){
         var thisLevel = this.props.levels.map(function(l){
-            return (<tr>
-            <td>{l.name}</td>
-            <td>
-                <Criteria criteria={l.criteria} />
-            </td>
-            </tr>)
+            return (
+              <tr key={l.name}>
+                <td>{l.name}</td>
+                <td>
+                  <Criteria criteria={l.criteria} />
+                </td>
+              </tr>
+            )
         });
         /*
-        <th scope="col" style={thWidth}>
-          Current<a style={anchorInfo} class="ibm-information-link" data-widget="tooltip" title="Your team's current maturity level."></a>
+        <th scope='col' style={thWidth}>
+          Current<a style={anchorInfo} class='ibm-information-link' data-widget='tooltip' title='Your team's current maturity level.'></a>
         </th>
-        <th scope="col" style={thWidth}>
-          Target<a style={anchorInfo} class="ibm-information-link" data-widget="tooltip" title="Your team's targets for the next 90 days.  Choose the practices that the team agrees will have the most impact."></a>
+        <th scope='col' style={thWidth}>
+          Target<a style={anchorInfo} class='ibm-information-link' data-widget='tooltip' title='Your team's targets for the next 90 days.  Choose the practices that the team agrees will have the most impact.'></a>
         </th>
         */
-        return (<div class="ibm-twisty-body" style={displayBlock}>
-        <table class="ibm-data-table ibm-altrows agile-practice" width="100%" summary="Maturity assessment level and description for the identified practice.">
+        return (<div class='ibm-twisty-body' style={displayBlock}>
+        <table class='ibm-data-table ibm-altrows agile-practice' width='100%' summary='Maturity assessment level and description for the identified practice.'>
           <caption>{this.props.description}</caption>
           <thead>
             <tr>
-              <th scope="col" colSpan="2">Maturity level</th>
+              <th scope='col' colSpan='2'>Maturity level</th>
             </tr>
           </thead>
           <tbody>
@@ -68,14 +70,14 @@ var Level = React.createClass({
 var SubTwistyContent = React.createClass({
     render: function(){
         var content = this.props.practices.map(function(con){
-            return(<li class="ibm-active">
-            <a class="ibm-twisty-trigger" href="#toggle">{con.name}</a>
+            return(<li class='ibm-active'>
+            <a class='ibm-twisty-trigger' href='#toggle'>{con.name}</a>
             <Level description={con.description} levels={con.levels} />
           </li>)
         });
-        return (<div class="ibm-twisty-body" style={displayBlock}>
-        <ul class="ibm-twisty agile-practice">
-         {content}  
+        return (<div class='ibm-twisty-body' style={displayBlock}>
+        <ul class='ibm-twisty agile-practice'>
+         {content}
         </ul>
       </div>)
     }
@@ -84,14 +86,16 @@ var SubTwistyContent = React.createClass({
 var SubTwisty = React.createClass({
     render: function(){
         var subTwistyContent = this.props.principles.map(function(content) {
-             return(<li class="ibm-active">
-              <a class="ibm-twisty-trigger" href="#toggle">{content.name}</a>
+          return(
+            <li class='ibm-active' key={content.name}>
+              <a class='ibm-twisty-trigger' href='#toggle'>{content.name}</a>
               <SubTwistyContent practices={content.practices} />
-            </li>)
+            </li>
+          );
         });
         console.log('containerTag: ', this.props.containerTag);
-        return (<div class="ibm-twisty-body" style={displayBlock} ref={this.props.containerTag}>
-          <ul class="ibm-twisty agile-principle">
+        return (<div class='ibm-twisty-body' style={displayBlock} ref={this.props.containerTag}>
+          <ul class='ibm-twisty agile-principle'>
             {subTwistyContent}
           </ul>
         </div>)
@@ -135,23 +139,25 @@ var AssessmentActiveTemplates = React.createClass({
         var cnt = 1;
         var visibility = this.state.childVisible;
         var componentName = this.state.activeTemplates['components'].map(function(tpl){
-            var childTag = 'firstChild' + cnt;
-            var indicator = 'i' + childTag;
-            cnt++;
-            return(<li id="{tplId}" class="" ref={indicator}>
-            <a class="ibm-twisty-trigger" id={childTag} onClick={toggleChild} href="#toggle">{tpl.name}</a>
-            <div ref={childTag} style={visibility}>
-            <SubTwisty principles={tpl.principles}/>
-            </div>
-        </li>)
+          var childTag = 'firstChild' + cnt;
+          var indicator = 'i' + childTag;
+          cnt++;
+          return(
+            <li id={tplId} class='' ref={indicator} key={tplId}>
+              <a class='ibm-twisty-trigger' id={childTag} onClick={toggleChild} href='#toggle'>{tpl.name}</a>
+              <div ref={childTag} style={visibility}>
+                <SubTwisty principles={tpl.principles}/>
+              </div>
+            </li>
+          );
         });
     } else {
         var tplId = null;
         var componentName = null;
     }
-  
-    return (<div id="assessmentContainer" class="agile-maturity">
-        <ul class="ibm-twisty agile-assessment ibm-widget-processed">
+
+    return (<div id='assessmentContainer' class='agile-maturity'>
+        <ul class='ibm-twisty agile-assessment ibm-widget-processed'>
         {componentName}
         </ul>
         </div>)
--- public/js/components/assessment/AssessmentButtons.jsx
+++ public/js/components/assessment/AssessmentButtons.jsx
@@ -9,8 +9,8 @@ var AssessmentButtons = React.createClass({
     };
 
     var statusColor = { color : 'blue'};
-    
-    if(this.props.assessmentStatus == '')
+
+    if(this.props.assessmentStatus.status == '')
       var isVisible = { display: 'none'};
     else
       var isVisible = { display: 'block'};
@@ -19,14 +19,14 @@ var AssessmentButtons = React.createClass({
       <div>
         <div style={isVisible}>
           <span style={spanStyle}>
-            Team assessment status: <span style={statusColor}>{this.props.assessmentStatus}</span>
+            Team assessment status: <span style={statusColor}>{this.props.assessmentStatus.status}</span>
           </span>
         </div>
         <div class="ibm-btn-row" style={{"textAlign": "right"}}>
-          <input type="button" class="ibm-btn-pri ibm-btn-small" name="submitAssessBtn" value="Submit" disabled={this.props.disabledButtons} />
-          <input type="button" class="ibm-btn-sec ibm-btn-small" name="saveAssessBtn" value="Save as draft" disabled={this.props.disabledButtons} />
-          <input type="button" class="ibm-btn-sec ibm-btn-small" name="deleteAssessBtn" value="Delete draft" disabled={this.props.disabledButtons} />
-          <input type="button" class="ibm-btn-sec ibm-btn-small" name="cancelAssessBtn" value="Cancel current changes" disabled={this.props.disabledButtons} />
+          <input type="button" class="ibm-btn-pri ibm-btn-small" name="submitAssessBtn" value="Submit" disabled={this.props.assessmentStatus.disabledButtons[0]} />
+          <input type="button" class="ibm-btn-sec ibm-btn-small" name="saveAssessBtn" value="Save as draft" disabled={this.props.assessmentStatus.disabledButtons[1]} />
+          <input type="button" class="ibm-btn-sec ibm-btn-small" name="deleteAssessBtn" value="Delete draft" disabled={this.props.assessmentStatus.disabledButtons[2]} />
+          <input type="button" class="ibm-btn-sec ibm-btn-small" name="cancelAssessBtn" value="Cancel current changes" disabled={this.props.assessmentStatus.disabledButtons[3]} />
           <span id="progressIndicatorTop"></span>
         </div>
       </div>
