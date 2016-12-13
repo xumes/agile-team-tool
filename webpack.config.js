da7e04f8bbcb8b5219341c9da839348a3595c91a
descalhc <descalhc@ph.ibm.com>
2016-12-13 21:44:26 +0800

    removed es2015 for now.

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015'],
+          presets: ['react'],
           plugins: ['react-html-attrs']
         }
       }
