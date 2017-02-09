b43dda1d34453dacadf01ec524a25de58ce9aa2d
Jean C. Lam <jeanlam@us.ibm.com>
2017-02-08 19:45:58 -0600

    parent child hierarchy on add team popup

--- public/js/components/v3_home/HomeAddTeamHierarchyModal.jsx
+++ public/js/components/v3_home/HomeAddTeamHierarchyModal.jsx
@@ -9,24 +9,33 @@ var HomeTeamParentChildSelection = require('./HomeTeamParentChildSelection.jsx')
 var HomeAddTeamHierarchyModal = React.createClass({
   getInitialState: function() {
     return {
-      selparentList: 'none'
+      selparentList: 'none',
+      defaultParentObjects: [
+        {name: 'Parent of my currently selected team', id: 'parentOfSelected'},
+        {name: 'Peer of my currently selected team', id: 'peerOfSelected'}
+      ]
     }
   },
 
   componentDidMount: function() {
     this.setState({selparentList: 'none'});
   },
-
   addTeamHandler: function() {
     var self = this;
   },
 
   render: function () {
     var self = this;
-
     var noteStyle = {
        color: '#4178BE'
     };
+
+   var populateDefaultParentOption = this.state.defaultParentObjects.map(function(item) {
+     console.log ('IN Populate default Parent Option');
+     return ( 
+      <option key={item._id} value={item._id}>{item.name}</option>
+     ) ;
+    });
 
     var populateTeamNames = this.props.teamNames.map(function(item) {
      return (
@@ -46,15 +55,11 @@ var HomeAddTeamHierarchyModal = React.createClass({
 
           <div class='new-team-creation-add-block-content'>
             <div class='new-team-creation-add-block-content-mid'>
-
               <div class='top-note-parent-child-hierarchy'>Please choose the parent team, if any, "above" your team as well as the childrean beneath.</div>
-
-              <HomeTeamParentChildSelection onchangeParentHierchSel={self.props.onchangeParentHierchSel} populateTeamNames={populateTeamNames}/>
-
-
+              <HomeTeamParentChildSelection onchangeParentHierchSel={self.props.onchangeParentHierchSel} populateTeamNames={populateTeamNames} populateDefaultParentOption={populateDefaultParentOption}/>
             </div>
           </div>
-
+          
           <HomeTeamHierarchyFooter updateStep={self.addTeamHandler} />  
 
         </div>   
--- public/js/components/v3_home/HomeTeamParentChildSelection.jsx
+++ public/js/components/v3_home/HomeTeamParentChildSelection.jsx
@@ -8,45 +8,49 @@ var HomeTeamParentChildSelection = React.createClass({
     $("select").select2(); 
    },
 
-  render: function() {
+  render: function() {        
+    var self = this;
     var selparent1Style = {'display': 'block'};
 
     return (
       <div class="pncteam-midcontent">
         <div class="optbox-1 rpad">
-
+        
           <div class="pncteam-bg">
             <InlineSVG src={require('../../../img/Att-icons/att-icons_parent+squad.svg')}></InlineSVG>
           </div>
           <div class='team-parent-child-hierarchy-p'>  
 
-             <div class='line1' style={{'width':130}}>
+             <div class='line1' style={{'width':150}}>
+                <InlineSVG src={require('../../../img/Att-icons/att-icons_arrow-thin-grey.svg')}></InlineSVG>
              </div>
-             <div class='pointer-arrow-right-parent'>
-                <InlineSVG src={require('../../../img/Att-icons/play-arrow.svg')}></InlineSVG>
-             </div>
-
+    
              <div class="optsel-parent" style={selparent1Style} >     
-               <select name="pc-hier-selparent" id="pc-hier-selparent" class="pc-hier-selparent" onClick={this.props.onchangeParentHierchSel}>
-                <option>Select parent team</option>
+               <select name="pc-hier-selparent" id="pc-hier-selparent" class="pc-hier-selparent" onChange={self.props.onchangeParentHierchSel} >
+                <option key='NA' value='NA'>Select parent team</option>
+                <option key='NoParent' value='NoParent'>Top tier / No Parent Above / Not Listed</option>
+                 {this.props.populateDefaultParentOption}
                  {this.props.populateTeamNames}
                 </select>
             </div>
 
+            <div class='line2' style={{'width':150}}>
+               <InlineSVG src={require('../../../img/Att-icons/att-icons_arrow-thin-grey.svg')}></InlineSVG>
+            </div>
 
-             <div class='line2' style={{'width':130}}>
-             </div>
-             <div class='pointer-arrow-right-current'>
-                <InlineSVG src={require('../../../img/Att-icons/play-arrow.svg')}></InlineSVG>
-             </div>
+            <div class="curteam-block">Current Team (being made)</div>
 
-             <div class='line3' style={{'width':90}}>
-             </div>
-             <div class='pointer-arrow-right-child'>
-                <InlineSVG src={require('../../../img/Att-icons/play-arrow.svg')}></InlineSVG>
+             <div class='line3' style={{'width':143}}>
+                <InlineSVG src={require('../../../img/Att-icons/att-icons_arrow-thin-grey.svg')}></InlineSVG>
              </div>
 
-
+             <div class="optsel-child" style={selparent1Style} >     
+               <select name="pc-hier-selChild" id="pc-hier-selChild" class="pc-hier-selChild" onClick={this.props.onchangeParentHierchSel}>
+                <option value='NA'>Add children team(s)</option>
+                <option value='NoChild'>Not Listed</option>
+                 {this.props.populateTeamNames}
+                </select>
+            </div>
 
 
           </div>
