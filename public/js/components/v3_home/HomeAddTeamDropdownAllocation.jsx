9ae34e77a23bb1ea26981cc86abd21681f20e8ac
Arman Ortega <ortegaaa@ph.ibm.com>
2017-02-28 02:39:19 +0800

    Updates for Team role/allocation modal

---
+++ public/js/components/v3_home/HomeAddTeamDropdownAllocation.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+var Select = require('react-select');
+
+var HomeAddTeamDropdownAllocation = React.createClass({
+
+  allocHandler: function(data) {
+    this.props.allocHandler(this.refs, data);
+  },
+  render: function() {
+    var self = this;
+    var memberUserId = self.props.memberUserId;
+    var memberAlloc = self.props.memberAlloc || 100;
+    var selallocOptions = [];
+    var allocationArray = Array.from(Array(101).keys());
+    allocationArray.map(function(v) {
+      selallocOptions.push({value: v, label: v});
+    });  
+
+    return(
+      <Select
+        name='select-alloc'
+        ref='selalloc'
+        data-uid={memberUserId}
+        value={memberAlloc}
+        options={selallocOptions}
+        clearable={false}
+        onChange={self.allocHandler} />
+    );
+  }
+});
+
+module.exports = HomeAddTeamDropdownAllocation;
