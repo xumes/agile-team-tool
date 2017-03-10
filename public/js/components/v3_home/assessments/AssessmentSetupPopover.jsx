94b5c2c87a2311dd34bf8fc8d470a546080aae75
Yanliang Gu <Yanliang.Gu1@ibm.com>
2017-03-10 08:29:28 -0500

    working on assessment setup

---
+++ public/js/components/v3_home/assessments/AssessmentSetupPopover.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactModal = require('react-modal');
+
+var AssessmentSetupPopover = React.createClass({
+  render: function() {
+    return (
+      <div tabIndex='1' class='assessment-setup'>
+      </div>
+    )
+  }
+});
+module.exports = AssessmentSetupPopover;
