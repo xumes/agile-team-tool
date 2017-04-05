067282558fb8f70814eaf25c7b6fb290008009a1
Yanliang Gu <Yanliang.Gu1@ibm.com>
2017-04-05 10:39:08 -0400

    init

--- routes/server/home.js
+++ routes/server/home.js
@@ -14,10 +14,6 @@ module.exports = function(app, includes) {
 
   app.get(['/', '/home'], includes.middleware.auth.requireLoginWithRedirect, function(req, res) {
     json['user'] = req.session.user;
-    render(req, res, 'v2_home', json);
-  });
-  app.get('/_v3_home', includes.middleware.auth.requireLoginWithRedirect, function(req, res) {
-    json['user'] = req.session.user;
     render(req, res, 'v3_home', json);
   });
 };
