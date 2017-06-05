8c42c1fd2c27ba34e3784d3d3d69b53eeb56f086
descalhc <descalhc@ph.ibm.com>
2017-06-06 02:48:36 +0800

    fix for duplicate image showing on header memmber summary on add member

--- public/js/components/index/HomeAddMember.jsx
+++ public/js/components/index/HomeAddMember.jsx
@@ -117,10 +117,14 @@ var HomeAddMember = React.createClass({
                   'workTime': $('#teamMemberAwkSelect').val() == 'other'?$('#otherAwk').val():$('#teamMemberAwkSelect').val()
                 };
                 newTeamMembers = JSON.parse(JSON.stringify(self.props.loadDetailTeam.team.members));
-                if (!_.isEmpty(self.props.loadDetailTeam.members))
-                  newUsers = JSON.parse(JSON.stringify(self.props.loadDetailTeam.members));
                 newTeamMembers.push(ntm);
-                newUsers.push(result);
+                if (!_.isEmpty(self.props.loadDetailTeam.members)) {
+                  newUsers = JSON.parse(JSON.stringify(self.props.loadDetailTeam.members));
+                  if (_.isEmpty(_.findWhere(newUsers, {userId: result.userId})))
+                    newUsers.push(result);
+                } else
+                    newUsers.push(result);
+
                 return api.modifyTeamMembers(self.props.loadDetailTeam.team._id, newTeamMembers);
               })
               .then(function(result){
