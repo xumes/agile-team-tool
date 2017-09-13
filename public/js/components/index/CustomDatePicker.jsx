fe9fd19116856c91ba0e380c2242ff7de0a82ffc
Steven Black <sblack@us.ibm.com>
2017-09-13 17:29:20 -0400

    Updated `CustomDatePicker` and `AssessmentDatePicker` to use `prop-types` module, and updated the coding style

--- public/js/components/index/CustomDatePicker.jsx
+++ public/js/components/index/CustomDatePicker.jsx
@@ -1,25 +1,35 @@
-var React = require('react');
-var api = require('../api.jsx');
-var InlineSVG = require('svg-inline-react');
-var ReactModal = require('react-modal');
-var moment = require('moment');
-var DatePicker = require('react-datepicker');
+const React = require('react');
+const PropTypes = require('prop-types');
+const InlineSVG = require('svg-inline-react');
+const calendarIcon = require('../../../img/Att-icons/att-icons_calendar.svg');
 
-var CustomDatePicker = React.createClass({
-
+const CustomDatePicker = React.createClass({
   propTypes: {
-onClick: React.PropTypes.func,
-value: React.PropTypes.string
-},
-render: function() {
-      return (
-        <div className='home-iter-new-dates'>
-          <input type="text" className='home-iter-date' id={this.props.fieldId} readOnly onClick={this.props.onClick} value={this.props.value}/>
-          <div class='home-iter-cal-btn-block' onClick={this.props.onClick}>
-            <InlineSVG src={require('../../../img/Att-icons/att-icons_calendar.svg')}></InlineSVG>
-          </div>
+    onClick: PropTypes.func,
+    value: PropTypes.string,
+  },
+
+  render() {
+    return (
+      <div className="home-iter-new-dates">
+        <input
+          type="text"
+          className="home-iter-date"
+          id={this.props.fieldId}
+          readOnly
+          onClick={this.props.onClick}
+          value={this.props.value}
+        />
+        <div
+          className="home-iter-cal-btn-block"
+          onClick={this.props.onClick}
+          role="button"
+          tabIndex="0"
+        >
+          <InlineSVG src={calendarIcon} />
         </div>
-      )
-  }
+      </div>
+    )
+  },
 });
 module.exports = CustomDatePicker;
