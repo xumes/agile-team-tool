a4b1fae433e656a18046a006fc94cdda37d8d7df
Steven Black <sblack@us.ibm.com>
2017-09-13 23:13:37 -0400

    Updated `uniqueID` utility to generate an ID based on a timestamp

--- public/js/util/uniqueID.jsx
+++ public/js/util/uniqueID.jsx
@@ -2,36 +2,23 @@
 let count = 0;
 
 /**
- * Function to create a unique ID. A counter is appended to the end of each
+ * Function to create a unique ID based on a timestamp. A counter is appended to the end of each
  * generated ID. This will all but guarantee true uniqueness of the ID.
  *
- * @param {integer} len The length of the ID (default is 5)
- * @param {string} chars The string of characters to use to generate the random ID (default A-Za-z)
- * @return {string} A random string based on parameters
+ * @return {string} A unique timestamp
  *
  * @example
  * const uniqueID = require('../util/uniqueID.jsx');
- * // Default usage
+ *
  * const aria_id = uniqueID();
- * // → "fAwfG-1"
+ * // → "1505358003471-1"
  *
- * @example
- * const uniqueID = require('../util/uniqueID.jsx');
- * // Usage with parameters
- * const item_id = uniqueID(6,"1234567890abcdef.!$");
- * // → "5.fd!2-2"
  */
-const uniqueID = (
-  len = 5,
-  chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
-) => {
+const uniqueID = () => {
+  const timestamp = Date.now();
   count += 1;
 
-  const id = Array.apply(0, Array(len))
-    .map(() => chars.charAt(Math.floor(Math.random() * chars.length)))
-    .join('');
-
-  return `${id}-${count}`;
+  return `${timestamp}-${count}`;
 };
 
 module.exports = uniqueID;
