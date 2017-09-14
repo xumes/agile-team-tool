3d600067bac28343b2ade9556f690e731ace7334
Steven Black <sblack@us.ibm.com>
2017-09-14 19:44:31 -0400

    uniqueID: added specs

--- karma.conf.js
+++ karma.conf.js
@@ -1,37 +1,37 @@
-module.exports = function (config) {
+module.exports = (config) => {
   config.set({
-    browsers: [ 'ChromeHeadless' ],
+    browsers: ['ChromeHeadless'],
     singleRun: true,
-    frameworks: [ 'jasmine' ],
+    frameworks: ['jasmine'],
     files: [
       'public/**/__tests__/unit/**/*spec.js',
     ],
     preprocessors: {
-      'public/*spec.js': [ 'webpack', 'sourcemap' ],
-      'public/**/*spec.js': [ 'webpack', 'sourcemap' ]
+      'public/*spec.js': ['webpack', 'sourcemap'],
+      'public/**/*spec.js': ['webpack', 'sourcemap'],
     },
-    reporters: [ 'dots' ],
+    reporters: ['dots'],
     webpack: {
       devtool: 'inline-source-map',
       module: {
         rules: [
           {
             test: /\.js$/,
-            exclude: /node_modules/,            
+            exclude: /node_modules/,
             use: [
               {
                 loader: 'babel-loader',
-                options: {                  
-                  presets: ['es2017', 'react'],                
+                options: {
+                  presets: ['es2017', 'react'],
                 },
               },
             ],
-          }
-        ]
-      }
+          },
+        ],
+      },
     },
     webpackServer: {
-      noInfo: true //please don't spam the console when running in karma!
-    }
+      noInfo: true, // please don't spam the console when running in karma!
+    },
   });
 };
---
+++ public/js/util/__tests__/unit/uniqueID.spec.js
@@ -0,0 +1,53 @@
+const uniqueID = require('../../uniqueID.js');
+
+fdescribe('Utility: uniqueID', () => {
+  it('is defined', () => {
+    expect(uniqueID).toBeDefined();
+  });
+
+  it('is a function', () => {
+    expect(typeof uniqueID).toBe('function');
+  });
+
+  it('can be run without error', () => {
+    expect(() => {
+      uniqueID();
+    }).not.toThrow();
+  });
+
+  it('returns a string', () => {
+    const test = uniqueID();
+    expect(typeof test).toBe('string');
+  });
+
+  it('string has two main parts', () => {
+    expect(uniqueID().split('-').length).toBe(2);
+  });
+
+  it('first part is a timestamp', () => {
+    // setup
+    jasmine.clock().install();
+    const change = 1000;
+    const baseTime = new Date(2013, 9, 23);
+    jasmine.clock().mockDate(baseTime);
+
+    // first
+    let test = uniqueID().split('-')[0];
+    expect(test).toEqual((baseTime.getTime()).toString());
+
+    // second
+    jasmine.clock().tick(change);
+    test = uniqueID().split('-')[0];
+    expect(test).toEqual((baseTime.getTime() + change).toString());
+
+    // teardown
+    jasmine.clock().uninstall();
+  });
+
+  it('second part is a count', () => {
+    const initial = parseFloat(uniqueID().split('-')[1]);
+    const test = parseFloat(uniqueID().split('-')[1]);
+
+    expect(test).toBe(initial + 1);
+  });
+});
