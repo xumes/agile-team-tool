b6917195484e206a724ecd115c66082de97bcee2
Steven Black <sblack@us.ibm.com>
2017-09-14 20:35:32 -0400

    Updated Karma config to allow `.jsx`

--- karma.conf.js
+++ karma.conf.js
@@ -1,37 +1,49 @@
-module.exports = function (config) {
+module.exports = (config) => {
   config.set({
-    browsers: [ 'ChromeHeadless' ],
+    browsers: ['ChromeHeadless'],
     singleRun: true,
-    frameworks: [ 'jasmine' ],
+    frameworks: ['jasmine'],
     files: [
       'public/**/__tests__/unit/**/*spec.js',
     ],
     preprocessors: {
-      'public/*spec.js': [ 'webpack', 'sourcemap' ],
-      'public/**/*spec.js': [ 'webpack', 'sourcemap' ]
+      'public/*spec.js': ['webpack', 'sourcemap'],
+      'public/**/*spec.js': ['webpack', 'sourcemap'],
     },
-    reporters: [ 'dots' ],
+    reporters: ['dots'],
     webpack: {
       devtool: 'inline-source-map',
       module: {
         rules: [
           {
-            test: /\.js$/,
-            exclude: /node_modules/,            
+            test: /\.jsx$|\.js$/,
+            exclude: /node_modules/,
             use: [
               {
                 loader: 'babel-loader',
-                options: {                  
-                  presets: ['es2017', 'react'],                
+                options: {
+                  presets: ['es2017', 'react'],
                 },
               },
             ],
-          }
-        ]
-      }
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              { loader: 'style-loader' },
+              { loader: 'css-loader' },
+              { loader: 'sass-loader' },
+            ],
+          },
+          {
+            test: /\.svg$/,
+            loader: 'svg-inline-loader',
+          },
+        ],
+      },
     },
     webpackServer: {
-      noInfo: true //please don't spam the console when running in karma!
-    }
+      noInfo: true, // please don't spam the console when running in karma!
+    },
   });
 };
