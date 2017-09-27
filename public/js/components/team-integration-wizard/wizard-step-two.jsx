f6db4ef43442d8fbe97468fe6a43eec2af5cd7a3
Dayoung Kim <dayoungkim@us.ibm.com>
2017-09-27 12:34:54 -0400

    totally customized dropdown that doesnt use select2 - jquery

--- public/js/components/team-integration-wizard/wizard-step-two.jsx
+++ public/js/components/team-integration-wizard/wizard-step-two.jsx
@@ -1,5 +1,7 @@
 const React = require('react');
 const propTypes = require('./prop-types');
+const InlineSVG = require('svg-inline-react');
+const dropdownIcon = require('../../../img/Att-icons/att-icons_show.svg');
 
 const WizardStepTwo = (props) => {
   const toolId = props.tools && props.tools.length ? props.tools[0].toolId : 0;
@@ -16,21 +18,34 @@ const WizardStepTwo = (props) => {
         <span htmlFor="your-squad">Your Agile Team Tool Squad</span>
         <p><input type="text" value={teamName} /></p>
 
-        <span className="att-integration__label">
+        {/* <span className="att-integration__label">
           {toolId} Server
         </span>
         <select className="att-integration__dropdown">
           <option defaultValue={server}>
             {server}
           </option>
-        </select>
+        </select> */}
 
-        <span className="att-integration__label">
-          {toolId} Project Area
-        </span>
-        <select className="att-integration__dropdown">
-          <option defaultValue={projectName}>{projectName}</option>
-        </select>
+        <span>{toolId} Server</span>
+        <div className="dropdown">
+          <button className="dropdown-btn">{server}
+            <InlineSVG src={dropdownIcon} />
+          </button>
+          <div className="dropdown-content">
+            <option>{server}</option>
+          </div>
+        </div>
+
+        <span>{toolId} Project Area</span>
+        <div className="dropdown">
+          <button className="dropdown-btn">{projectName}
+            <InlineSVG src={dropdownIcon} />
+          </button>
+          <div className="dropdown-content">
+            <option>{projectName}</option>
+          </div>
+        </div>
       </div>
     </div>
   );
