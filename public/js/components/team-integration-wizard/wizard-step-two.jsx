c514345c1a30413c4a8e03ff0b866bb416b9bd6a
Dayoung Kim <dayoungkim@us.ibm.com>
2017-09-27 16:34:47 -0400

    responsive step one and two

--- public/js/components/team-integration-wizard/wizard-step-two.jsx
+++ public/js/components/team-integration-wizard/wizard-step-two.jsx
@@ -3,53 +3,62 @@ const propTypes = require('./prop-types');
 const InlineSVG = require('svg-inline-react');
 const dropdownIcon = require('../../../img/Att-icons/att-icons_show.svg');
 
-const WizardStepTwo = (props) => {
-  const toolId = props.tools && props.tools.length ? props.tools[0].toolId : 0;
-  const teamName = props.team ? props.team.name : '';
-  const projectName = props.projects && props.projects.length ? props.projects[0].projectName : '';
-  const server = props.team.integration ? props.team.integration.server : '';
+class WizardStepTwo extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="att-integration">
-      <h2 className="att-integration__heading-step">
-        Step 2 of 4: Locate your {toolId} team
-      </h2>
-      <div className="att-integration__container">
-        <span htmlFor="your-squad">Your Agile Team Tool Squad</span>
-        <p><input type="text" value={teamName} /></p>
+    this.state = {
+    };
 
-        {/* <span className="att-integration__label">
-          {toolId} Server
-        </span>
-        <select className="att-integration__dropdown">
-          <option defaultValue={server}>
-            {server}
-          </option>
-        </select> */}
+    this.props = props;
+    this.updateText = this.updateText.bind(this);
+  }
 
-        <span>{toolId} Server</span>
-        <div className="dropdown">
-          <button className="dropdown-btn">{server}
-            <InlineSVG src={dropdownIcon} />
-          </button>
-          <div className="dropdown-content">
-            <option>{server}</option>
+  updateText() {
+    this.setState({
+    });
+  }
+
+  render() {
+    const props = this.props;
+    const toolId = props.tools && props.tools.length ? props.tools[0].toolId : 0;
+    const teamName = props.team ? props.team.name : '';
+    const projectName = props.projects && props.projects.length ? props.projects[0].projectName : '';
+    const server = props.team.integration ? props.team.integration.server : '';
+
+    return (
+      <div className="att-integration">
+        <h2 className="att-integration__heading-step">
+          Step 2 of 4: Locate your {toolId} team
+        </h2>
+        <div className="att-integration__container">
+          <span htmlFor="your-squad">Your Agile Team Tool Squad</span>
+          <input type="text" value={teamName} />
+
+          <span>{toolId} Server</span>
+          <div className="dropdown">
+            <button className="dropdown-btn" onClick={this.updateText}>{server}
+              <InlineSVG src={dropdownIcon} />
+            </button>
+            <div className="dropdown-content">
+              <option>{server}</option>
+            </div>
           </div>
-        </div>
 
-        <span>{toolId} Project Area</span>
-        <div className="dropdown">
-          <button className="dropdown-btn">{projectName}
-            <InlineSVG src={dropdownIcon} />
-          </button>
-          <div className="dropdown-content">
-            <option>{projectName}</option>
+          <span>{toolId} Project Area</span>
+          <div className="dropdown">
+            <button className="dropdown-btn">{projectName}
+              <InlineSVG src={dropdownIcon} />
+            </button>
+            <div className="dropdown-content">
+              <option>{projectName}</option>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 WizardStepTwo.propTypes = propTypes.types;
 WizardStepTwo.defaultProps = propTypes.defaults;
