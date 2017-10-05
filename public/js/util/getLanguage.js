a55ddab5fead6df51dcfb2f7035ed1424f022b42
Steven Black <sblack@us.ibm.com>
2017-10-05 19:00:49 -0400

    getLanguage: refactored to be more robust.. Following Angular approach.

--- public/js/util/getLanguage.js
+++ public/js/util/getLanguage.js
@@ -22,27 +22,56 @@ const urlParams = require('./urlParams');
  *
  */
 const getLanguage = () => {
-  const language = (
-    window.navigator.userLanguage ||
-    window.navigator.language
-  ).toLowerCase();
+  // navigator.languages:    Chrome & FF
+  // navigator.language:     Safari & Others
+  // navigator.userLanguage: IE & Others
+  const nav = window.navigator;
+  const langKeys = [
+    'language',
+    'browserLanguage',
+    'systemLanguage',
+    'userLanguage',
+  ];
+  let language;
+  let result;
 
-  let lang = null;
+  /**
+   * Get the Language
+   */
+  if (Array.isArray(nav.languages)) {
+    language = nav.languages.filter(Boolean)[0];
+  }
 
+  if (!language) {
+    language = langKeys
+      .map(v => nav[v])
+      .filter(Boolean)[0];
+  }
+
+
+  /**
+   * Lowercase, fallback to empty string to avoid potential Error
+   */
+  language = (language || '').toLowerCase();
+
+
+  /**
+   * Process the Language and any overrides
+   */
   if (language.indexOf('-') > -1) {
     const split = language.split('-');
-    lang = {
+    result = {
       lc: urlParams.get('lc') || split[0],
       cc: urlParams.get('cc') || split[1],
     };
   } else {
-    lang = {
-      lc: urlParams.get('lc') || language,
+    result = {
+      lc: urlParams.get('lc') || language || null,
       cc: urlParams.get('cc') || null,
     };
   }
 
-  return lang;
+  return result;
 };
 
 module.exports = getLanguage;
